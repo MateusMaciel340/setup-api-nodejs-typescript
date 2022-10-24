498564f952d3ad7a65ed30367961dec7e57168ad
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-24 15:55:13 -0300

    feat: adding imports needed to run application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
 import express from "express";
+import * as helmet from "helmet";
+import rateLimit, { MemoryStore } from "express-rate-limit";
+import cors from "cors";
+
+import helloWorld from "./controllers/hello.controller";
 
 const app = express();
 
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.send({ pong: "Hello World!" });
+const globalRateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: false,
+    store: new MemoryStore(),
 });
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    const message = helloWorld();
+
+    res.send({ pong: message });
+});
+
+app.use(helmet.expectCt({
+    maxAge: 86400,
+    enforce: true
+}));
+
+app.use(
+    helmet.frameguard(), helmet.hidePoweredBy(),
+    helmet.hsts(), helmet.noSniff(), helmet.permittedCrossDomainPolicies(),
+    cors()
+);
+
+app.use("/*", globalRateLimiter);
 
 export default app;
